1ceb9fbb4c1f26d9c301e5cee9fd2a2d052ac84c
Emil Quluzade <125368475+EmilQuluzade@users.noreply.github.com>
2024-01-30 11:04:26 +0400

    Challange 4

---
+++ Leetcode#4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = { "flower", "flow", "flight" };
+
+            if (strs == null || strs.Length == 0)
+                Console.WriteLine("");
+
+            // Take the first string as the reference for comparison
+            string prefix = strs[0];
+
+            for (int i = 1; i < strs.Length; i++)
+            {
+                // Compare characters of the reference string with corresponding characters of other strings
+                int j = 0;
+                while (j < prefix.Length && j < strs[i].Length && prefix[j] == strs[i][j])
+                {
+                    j++;
+                }
+                prefix = prefix.Substring(0, j); // Update prefix to the common part
+            }
+            Console.WriteLine(prefix);
+        }
+    }
+}
